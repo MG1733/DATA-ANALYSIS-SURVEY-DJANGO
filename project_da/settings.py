bbb0e90f129ddb6e0515b90e2465bb285c396f97
MG1733 <mohanaganesh1733@gmail.com>
2025-12-07 10:13:16 +0530

    Configured PostgreSQL for Render deployment

--- project_da/settings.py
+++ project_da/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +74,15 @@ WSGI_APPLICATION = 'project_da.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        "postgresql://postgres:Mohan%241733@db.abcdefghijklmnopqrst.supabase.co:5432/postgres",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
